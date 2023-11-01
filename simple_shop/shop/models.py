5facb9da2003c57aea0e9e684b924d3023267449
Artur Wrzosek <wrzosek.artur@gmail.com>
2023-11-01 21:51:04 +0100

    changing some relations in models

--- simple_shop/shop/models.py
+++ simple_shop/shop/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-from permissions import BUYER_PERMISSIONS
+from django.contrib.auth.models import AbstractUser
 
 
-class BaseUser(User):
+class BaseUser(AbstractUser):
     BUYER = "BUYER"
     SELLER = "SELLER"
     ROLE_CHOICES = (
@@ -12,15 +10,10 @@ class BaseUser(User):
         (SELLER, "Seller"),
     )
 
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
     role = models.CharField(max_length=15, choices=ROLE_CHOICES)
 
-
-class Buyer(BaseUser):
-
-    class Meta:
-        permissions = BUYER_PERMISSIONS
+    def __str__(self):
+        return f"{self.role}: {self.username}"
 
 
 class BaseModel(models.Model):
@@ -33,30 +26,36 @@ class BaseModel(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Product(BaseModel):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    category = models.ForeignKey(to=Category, on_delete=models.SET_NULL)
-    photo_url = models.ImageField()
-    photo_mini_url = models.ImageField()
+    category = models.ForeignKey(to=Category, related_name="products", on_delete=models.SET_NULL, null=True)
+    photo_url = models.ImageField(upload_to="photos/", blank=True, null=True)
+    photo_mini_url = models.ImageField(upload_to="photos/", width_field=200, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class OrderItem(BaseModel):
-    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     quantity = models.PositiveSmallIntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     summary_price = models.DecimalField(max_digits=6, decimal_places=2)
+    order_list = models.ForeignKey("OrderList", on_delete=models.CASCADE)
 
 
 class OrderList(BaseModel):
-    item = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
     final_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Order(BaseModel):
-    client = models.ForeignKey(to=BaseUser, on_delete=models.DO_NOTHING)
+    client = models.ForeignKey(to=BaseUser, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=100)
     order_list = models.OneToOneField(to=OrderList, on_delete=models.CASCADE)
     payment_date = models.DateField()
