5d457a96c178057f481cb7faa9b71b8d0d03583c
Artur Wrzosek <wrzosek.artur@gmail.com>
2023-11-02 21:53:00 +0100

    adding reverse function for migration file

--- simple_shop/shop/migrations/0002_auto_20231030_1450.py
+++ simple_shop/shop/migrations/0002_auto_20231030_1450.py
@@ -2,46 +2,40 @@ from django.contrib.auth.models import Group, Permission
 from django.db import migrations
 from django.contrib.contenttypes.models import ContentType
 
-from ..models import Product, Order
-from ..permissions import (
-    PRODUCT_DELETE_PERMISSION,
-    PRODUCT_CREATE_PERMISSION,
-    PRODUCT_UPDATE_PERMISSION,
-    ORDER_CREATE_PERMISSION,
-    STATISTICS_VIEW_PERMISSION,
-)
+from ..permissions import BuyerGroupPermissions, SellerGroupPermissions
 
 
-roles = {
-    "BUYERS": {
-        Order: [
-            ORDER_CREATE_PERMISSION
-        ]
-    },
-    "SELLERS": {
-        Product: [
-            PRODUCT_CREATE_PERMISSION,
-            PRODUCT_UPDATE_PERMISSION,
-            PRODUCT_DELETE_PERMISSION,
-            # STATISTICS_VIEW_PERMISSION
-        ],
-    },
-}
+ROLE_PERMISSIONS = [
+    BuyerGroupPermissions,
+    SellerGroupPermissions
+]
 
 
-def add_group_permissions(apps, schema_editor):
-    for role in roles:
-        group, group_created = Group.objects.get_or_create(name=role)
-        for model, permissions in roles[role].items():
+def add_groups_with_permissions(apps, schema_editor):
+    """
+    Creating instances of Group classes with given list of GroupPermissions
+    :param apps:
+    :param schema_editor:
+    :return:
+    """
+    for role in ROLE_PERMISSIONS:
+        group, group_created = Group.objects.get_or_create(name=role.GROUP_NAME)
+        role_instance = role()
+        role_instance.set_permission_list()
+        for permission in role_instance.PERMISSION_LIST:
+            model = apps.get_model(f"{permission['app_name']}", f"{permission['model_name']}")
             content_type = ContentType.objects.get_for_model(model)
-            for perm in permissions:
-                permission, created = Permission.objects.get_or_create(
-                    name=perm[1],
-                    codename=perm[0],
-                    content_type=content_type
-                )
-                group.permissions.add(permission)
+            permission, created = Permission.objects.get_or_create(
+                name=permission["name"],
+                codename=permission["codename"],
+                content_type=content_type
+            )
+            group.permissions.add(permission)
         group.save()
+
+
+def revert_add_groups_migration(apps, schema_editor):
+    Group.objects.get(name__in=[r.GROUP_NAME for r in ROLE_PERMISSIONS]).delete()
 
 
 class Migration(migrations.Migration):
@@ -51,5 +45,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(add_group_permissions)
+        migrations.RunPython(add_groups_with_permissions, revert_add_groups_migration)
     ]
