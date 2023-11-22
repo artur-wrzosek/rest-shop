0ac875a7e531ce42c86833063564454f7268f6e7
Artur Wrzosek <wrzosek.artur@gmail.com>
2023-11-22 11:28:19 +0100

    adding permissions for the owner-creator of an object, minor fixes

--- simple_shop/shop/permissions.py
+++ simple_shop/shop/permissions.py
@@ -1,17 +1,6 @@
-from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
 from rest_framework.permissions import BasePermission
 
 from .apps import ShopConfig
-from .models import Product, Order
-
-PRODUCT_VIEW_PERMISSION = ("view_product", "Can view product")
-PRODUCT_LIST_PERMISSION = ("list_product", "Can view list of products")
-PRODUCT_CREATE_PERMISSION = ("create_product", "Can create product")
-PRODUCT_UPDATE_PERMISSION = ("update_product", "Can update product")
-PRODUCT_DELETE_PERMISSION = ("delete_product", "Can delete product")
-ORDER_CREATE_PERMISSION = ("create_order", "Can create order")
-STATISTIC_LIST_PERMISSION = ("list_statistic", "Can list statistic")
 
 
 class PermissionListMixin:
@@ -21,12 +10,14 @@ class PermissionListMixin:
     """
     MODELS_PERMISSIONS = {}
     APP_NAME = ""
+    PERMISSION_LIST = None
 
     def _build_permissions_list(self) -> list[dict]:
         permission_list = []
         for model, action_list in self.MODELS_PERMISSIONS.items():
             for action in action_list:
                 perm = {
+                    "model_name": f"{model}",
                     "codename": f"{action.lower()}_{model.lower()}",
                     "name": f"Can {action.lower()} {model.lower()}",
                     "app_name": f"{self.APP_NAME}",
@@ -35,37 +26,46 @@ class PermissionListMixin:
                 permission_list.append(perm)
         return permission_list
 
-    @property
-    def permission_list(self) -> list[dict]:
-        return self._build_permissions_list()
+    def set_permission_list(self):
+        self.PERMISSION_LIST = self._build_permissions_list()
 
 
 class BuyerGroupPermissions(BasePermission, PermissionListMixin):
+    GROUP_NAME = "Buyers"
     APP_NAME = ShopConfig.name
     MODELS_PERMISSIONS = {
         "Product": ["view", "list"],
         "Order": ["view", "add", "change"],
+        "Category": ["view", "list"],
     }
 
     def has_permission(self, request, view):
         user_permissions = request.user.get_all_permissions()
-        for perm in self.permission_list:
+        self.set_permission_list()
+        for perm in self.PERMISSION_LIST:
             if perm["permission"] not in user_permissions:
                 return False
         return True
 
 
 class SellerGroupPermissions(BasePermission, PermissionListMixin):
+    GROUP_NAME = "Sellers"
     APP_NAME = ShopConfig.name
     MODELS_PERMISSIONS = {
         "Product": ["view", "add", "change", "delete", "list"],
         "Order": ["view", "add", "change"],
-
+        "Category": ["view", "add", "change", "delete", "list"],
     }
 
     def has_permission(self, request, view):
         user_permissions = request.user.get_all_permissions()
-        for perm in self.permission_list:
+        self.set_permission_list()
+        for perm in self.PERMISSION_LIST:
             if perm["permission"] not in user_permissions:
                 return False
         return True
+
+
+class IsOwnerPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.created_by == request.user
